f291906ce53826b5a4de0cb9bbac09bd5e70aaaa
Enrique077 <enriquemena.xs@gmail.com>
2025-11-17 20:34:05 -0600

    ProblemasSimples1.c

--- ProblemasSimples1.c
+++ ProblemasSimples1.c
@@ -1,1 +1,22 @@
+/*Enrique Mena Hernández
+programa 1*/
+/*Escriba un diagrama de flujo tal que dado como datos dos n ́umeros y
+calcular la suma, resta y multiplicacion de dichos numeros.*/
 
+#include <stdio.h>
+
+int main() {
+    float a, b, suma, resta, multiplicacion;
+    printf("Ingrese el primer numero: ");
+    scanf("%f", &a);
+    printf("Ingrese el segundo numero: ");
+    scanf("%f", &b);
+    suma = a + b;
+    resta = a - b;
+    multiplicacion = a * b;
+    printf("La suma es: %.2f\n", suma);
+    printf("La resta es: %.2f\n", resta);
+    printf("La multiplicacion es: %.2f\n", multiplicacion);
+    return 0;
+}
+  
