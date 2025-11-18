557a6644eb0d98ec4124418486c35731ce0d3026
Enrique077 <enriquemena.xs@gmail.com>
2025-11-17 20:45:11 -0600

    ProblemasSimples2.c

---
+++ ProblemasSimples2.c
@@ -0,0 +1,21 @@
+/*Enrique Mena Hernández
+programa 2*/
+/*Construir un diagrama de flujo tal que dado el radio de un c ́ırculo, calcule
+e imprima el  ́area y la circunferencia. El  ́area del c ́ırculo se calcula como
+Area = π ∗ radio2 y la circunferencia se calcula como Circunferencia =
+2 ∗ π ∗ radio*/
+
+#include <stdio.h>
+#define PI 3.1416
+
+int main() 
+{
+    float radio, area, circunferencia;
+    printf("Ingrese el radio del circulo: ");
+    scanf("%f", &radio);
+    area = PI * radio * radio;
+    circunferencia = 2 * PI * radio;
+    printf("El area es: %.2f\n", area);
+    printf("La circunferencia es: %.2f\n", circunferencia);
+    return 0;
+}
