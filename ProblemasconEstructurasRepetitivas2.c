3e9b0b7599b6fdfeee792d2d346ada9da3baa79e
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 16:11:21 -0600

    ProblemasconEstructurasRepetitivas2.c

--- ProblemasconEstructurasRepetitivas2.c
+++ ProblemasconEstructurasRepetitivas2.c
@@ -1,18 +1,24 @@
-
+/*Enrique Mena Hernández
+programa 2*/
 /*Construya un diagrama de flujo que lea 100 numeros naturales y cuente
 cuantos de ellos son positivos, negativos o nulos.*/
-#include <stdio.h>
 
-int main() {
+#include <stdio.h>
+int main() 
+{
     int numero, positivos = 0, negativos = 0, nulos = 0;
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 1; i <= 100; i++) 
+    {
         printf("Ingrese el numero %d: ", i);
         scanf("%d", &numero);
-        if (numero > 0) {
+        if (numero > 0) 
+        {
             positivos++;
-        } else if (numero < 0) {
+        } else if (numero < 0) 
+        {
             negativos++;
-        } else {
+        } else 
+        {
             nulos++;
         }
     }
