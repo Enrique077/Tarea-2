c21e8024eaf84498b581e936ae4efc3a26ca2e0b
Enrique077 <enriquemena.xs@gmail.com>
2025-11-19 21:40:17 -0600

    ProblemasconArreglos2.c

---
+++ ProblemasconArreglos2.c
@@ -0,0 +1,28 @@
+
+/*construya un programa tal que dado como entrada un arreglo unidimen-
+sional de enteros y un n ́umero entero, determine cu ́antas veces se encuentra
+el n ́umero dentro del arreglo.*/
+
+#include <stdio.h>
+int main() {
+    int n, X, contador = 0;
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &n);
+    int arreglo[n];
+    // Leer los elementos del arreglo
+    for (int i = 0; i < n; i++) {
+        printf("Ingrese el elemento %d: ", i+1);
+        scanf("%d", &arreglo[i]);
+    }
+    // Leer el número que queremos buscar
+    printf("Ingrese el numero a buscar: ");
+    scanf("%d", &X);
+    // Contar cuántas veces aparece X en el arreglo
+    for (int i = 0; i < n; i++) {
+        if (arreglo[i] == X) {
+            contador++;
+        }
+    }
+    printf("El numero %d aparece %d veces en el arreglo.\n", X, contador);
+    return 0;
+}
