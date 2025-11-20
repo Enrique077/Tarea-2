f13aecdcd74d1d839e1cd76e1926e22c07b69aa3
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 16:27:29 -0600

    ProblemasconArreglos3.c

--- ProblemasconArreglos3.c
+++ ProblemasconArreglos3.c
@@ -1,24 +1,29 @@
-
+/*Enrique Mena Hernández
+programa 3*/
 /*Escribir un diagrama de flujo tal que dado como entrada un arreglo unidi-
 mensional de enteros, determinar cu ́antos de ellos son positivos, negativos
 o nulos.*/
 
 #include <stdio.h>
-int main() {
+int main() 
+{
     int n;
     printf("Ingrese el tamaño del arreglo: ");
     scanf("%d", &n);
     int arreglo[n];
     int positivos = 0, negativos = 0, nulos = 0;
-    // Leer los elementos del arreglo
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         printf("Ingrese el elemento %d: ", i+1);
         scanf("%d", &arreglo[i]);
-        if (arreglo[i] > 0) {
+        if (arreglo[i] > 0) 
+        {
             positivos++;
-        } else if (arreglo[i] < 0) {
+        } else if (arreglo[i] < 0) 
+        {
             negativos++;
-        } else {
+        } else 
+        {
             nulos++;
         }
     }
