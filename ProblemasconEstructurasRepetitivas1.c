d96a35edcc587b9d92e0951d5b276d0d4307dea9
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 16:05:58 -0600

    ProblemasconEstructurasRepetitivas1.c

--- ProblemasconEstructurasRepetitivas1.c
+++ ProblemasconEstructurasRepetitivas1.c
@@ -1,13 +1,16 @@
-
+/*Enrique Mena Hernández
+programa 1*/
 /*Haga un diagrama de flujo para obtener la tabla de multiplicar de un
 n ́umero entero N, comenzando desde el 1.*/
 
 #include <stdio.h>
-int main() {
+int main() 
+{
     int N, i = 1;
     printf("Ingrese un numero entero: ");
     scanf("%d", &N);
-    while (i <= 10) {
+    while (i <= 10) 
+    {
         printf("%d x %d = %d\n", N, i, N * i);
         i++;
     }
