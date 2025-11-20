21c47d7821c9507b713da204a508341b224f68e1
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 16:12:57 -0600

    ProblemasconEstructurasRepetitivas3.c

--- ProblemasconEstructurasRepetitivas3.c
+++ ProblemasconEstructurasRepetitivas3.c
@@ -1,12 +1,16 @@
-
+/*Enrique Mena Hernández
+programa 2*/
 /*Hacer un diagrama de flujo que calcule la suma de los n ́umeros pares
 comprendidos entre el 10 y 50.*/
 
 #include <stdio.h>
-int main() {
+int main() 
+{
     int suma = 0;
-    for (int numero = 10; numero <= 50; numero++) {
-        if (numero % 2 == 0) {
+    for (int numero = 10; numero <= 50; numero++) 
+    {
+        if (numero % 2 == 0) 
+        {
             suma += numero;
         }
     }
