0e5aca0528d21d6d97ad8ebb3595732417285f4a
Enrique077 <enriquemena.xs@gmail.com>
2025-11-19 21:36:12 -0600

    ProblemasconArreglos1.c

---
+++ ProblemasconArreglos1.c
@@ -0,0 +1,20 @@
+
+/*Escribir un diagrama de flujo tal que dado como entrada un arreglo uni-
+dimensional de enteros, obtenga como resultado la suma de los mismos.*/
+
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &n);
+    int arreglo[n];
+    int suma = 0;
+    // Leer los elementos del arreglo
+    for (int i = 0; i < n; i++) {
+        printf("Ingrese el elemento %d: ", i+1);
+        scanf("%d", &arreglo[i]);
+        suma += arreglo[i]; // vamos sumando cada elemento
+    }
+    printf("La suma de los elementos del arreglo es: %d\n", suma);
+    return 0;
+}
