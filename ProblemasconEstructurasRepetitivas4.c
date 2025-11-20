9524629404680446373808d0acd2953d335f24cc
Enrique077 <enriquemena.xs@gmail.com>
2025-11-20 16:19:13 -0600

    ProblemasconEstructurasRepetitivas4.c

--- ProblemasconEstructurasRepetitivas4.c
+++ ProblemasconEstructurasRepetitivas4.c
@@ -1,13 +1,16 @@
-
+/*Enrique Mena Hernández
+programa 4*/
 /*Hacer un diagrama de flujo que calcule e imprima el producto de los N
 primeros n ́umeros naturales.*/
 
 #include <stdio.h>
-int main() {
+int main() 
+{
     int N, producto = 1;
     printf("Ingrese el valor de N: ");
     scanf("%d", &N);
-    for (int i = 1; i <= N; i++) {
+    for (int i = 1; i <= N; i++) 
+    {
         producto *= i;
     }
     printf("El producto de los %d primeros numeros naturales es: %d\n", N, producto);
