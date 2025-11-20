3ee7fef9e34bdfd00c3617380e4ecc353be3cc79
Enrique077 <enriquemena.xs@gmail.com>
2025-11-19 20:57:04 -0600

    ProblemasSimples3.c

--- ProblemasSimples3.c
+++ ProblemasSimples3.c
@@ -1,8 +1,11 @@
 /*Enrique Mena Hernández
 programa 3*/
+/*En una Casa de Cabio necesitan construir un programa que dado como
+dato una cantidad expresada en d ́olares, convierta esa cantidad en pesos.
+Construir el diagrama de flujo correspondiente. Tomar en cuenta que:
+1dolar ↢ 11.96pesos.*/
 
 #include <stdio.h>
-
 int main() 
 {
     float dolares, pesos;
